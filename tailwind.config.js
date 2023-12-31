1c41958bc5eb7730d15d276f40948a007627067e
Maxime <maxime.gouillardon@gmail.com>
2023-12-31 22:43:18 +0100

    feat(tailwind.config): add colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'bali-hai': {
+          '50': '#f8fafa',
+          '100': '#f1f4f6',
+          '200': '#e6ecee',
+          '300': '#d2dbe0',
+          '400': '#b8c5cd',
+          '500': '#8fa2b0',
+          '600': '#8294a5',
+          '700': '#6f8092',
+          '800': '#5d6c7a',
+          '900': '#4d5965',
+          '950': '#323b43',
+      },      
+      }
+    }
   },
   plugins: []
 }
