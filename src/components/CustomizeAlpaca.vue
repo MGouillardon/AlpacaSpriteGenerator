9e4a38c73a34101ba5792764b4dbfed7e7549e27
Maxime <maxime.gouillardon@gmail.com>
2024-01-01 15:46:09 +0100

    feat: add bg-color on item's button when selected

--- src/components/CustomizeAlpaca.vue
+++ src/components/CustomizeAlpaca.vue
@@ -5,12 +5,26 @@ import { ref } from 'vue'
 
 const alpaca = ref(alpacaConfig)
 const selectedCategory = ref(null)
-const selectedItems = ref([])
+const selectedItem = ref(null)
+const itemsForCategorySelected = ref([])
 
-const setSelectedItems = (category) => {
+const setItemsForCategorySelected = (category) => {
   selectedCategory.value = category.name
-  selectedItems.value = category.items
-  return category.items
+  itemsForCategorySelected.value = category.items.map((item) => capitalizeFirstLetter(item))
+  return itemsForCategorySelected.value
+}
+
+const setSelectedCategory = (category) => {
+  selectedCategory.value = category.name
+  itemsForCategorySelected.value = setItemsForCategorySelected(category)
+}
+
+const setSelectedItem = (item) => {
+  return (selectedItem.value = item)
+}
+
+const capitalizeFirstLetter = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
 }
 </script>
 
@@ -24,7 +38,7 @@ const setSelectedItems = (category) => {
           :key="key"
           :text="category.name"
           :class="{ 'bg-bali-hai-600 text-white': selectedCategory === category.name }"
-          @click="selectedItems = setSelectedItems(category)"
+          @click="setSelectedCategory(category)"
         />
       </div>
     </section>
@@ -32,11 +46,11 @@ const setSelectedItems = (category) => {
       <h2 class="text-2xl py-4">Style</h2>
       <div class="flex flex-wrap gap-2 font-primary">
         <ButtonPrimary
-          v-for="(item, key) in selectedItems"
+          v-for="(item, key) in itemsForCategorySelected"
           :key="key"
           :text="item"
-          :class="{ 'bg-bali-hai-600 text-white': selectedCategory === item }"
-          @click="selectedCategory = item"
+          :class="{ 'bg-bali-hai-600 text-white': selectedItem === item }"
+          @click="setSelectedItem(item)"
         />
       </div>
     </section>
