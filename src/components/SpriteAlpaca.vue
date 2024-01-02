2158064028e1032f11f2b658c266e2bb632c613d
Maxime <maxime.gouillardon@gmail.com>
2024-01-02 19:36:03 +0100

    feat: refactor img rendering in spriteAlpaca

--- src/components/SpriteAlpaca.vue
+++ src/components/SpriteAlpaca.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watchEffect } from 'vue'
+import { ref, watchEffect, computed } from 'vue'
 import { alpacaConfig } from '@/config/alpaca/alpaca'
 import ButtonPrimary from './UI/ButtonPrimary.vue'
 
@@ -10,16 +10,34 @@ const props = defineProps({
   }
 })
 
+const IMAGE_PATH = '/images/alpaca/';
 const sprite = ref({})
+
+
+const spriteParts = [
+  { name: 'accessories', zIndex: 40 },
+  { name: 'backgrounds', default: 'blue50' },
+  { name: 'ears', default: 'default' },
+  { name: 'eyes', zIndex: 30, default: 'default' },
+  { name: 'hair', zIndex: 20, default: 'default' },
+  { name: 'leg', default: 'default' },
+  { name: 'mouth', zIndex: 10, default: 'default' },
+  { name: 'neck', default: 'default' },
+  { name: 'nose', default: 'nose' },
+]
+
+const filteredSpriteParts = computed(() => {
+  return spriteParts.filter(part => part.name !== 'accessories' || sprite.value.accessories)
+})
 
 const downloadSprite = () => {
   const canvas = document.createElement('canvas')
-  const ctx = canvas.getContext('2d')
+  const contexte = canvas.getContext('2d')
 
   const parts = ['backgrounds', 'leg', 'neck', 'nose', 'mouth', 'ears', 'hair', 'eyes', 'accessories']
   const images = parts.map(part => {
     const img = new Image()
-    img.src = `/images/alpaca/${part}/${sprite.value[part] || 'default'}.png`
+    img.src = `${IMAGE_PATH}${part}/${sprite.value[part] || 'default'}.png`
     return img
   })
 
@@ -27,7 +45,7 @@ const downloadSprite = () => {
     .then(() => {
       canvas.width = images[0].width
       canvas.height = images[0].height
-      images.forEach(img => ctx.drawImage(img, 0, 0))
+      images.forEach(img => contexte.drawImage(img, 0, 0))
       const dataURL = canvas.toDataURL('image/png')
       const link = document.createElement('a')
       link.download = 'alpaca.png'
@@ -65,49 +83,11 @@ watchEffect(() => {
 <div class="h-full">
   <div class="relative h-[85%] w-auto flex items-center justify-center">
     <img
-      v-if="sprite.accessories"
-      class="absolute w-auto h-full z-40"
-      :src="`/images/alpaca/accessories/${sprite.accessories}.png`"
-      alt="Alpaca Sprite"
-    />
-    <img
-      class="absolute w-auto h-full rounded-xl"
-      :src="`/images/alpaca/backgrounds/${sprite.backgrounds || 'blue50'}.png`"
-      alt="Alpaca Sprite"
-    />
-    <img
+      v-for="(part, index) in filteredSpriteParts"
+      :key="index"
       class="absolute w-auto h-full"
-      :src="`/images/alpaca/ears/${sprite.ears || 'default'}.png`"
-      alt="Alpaca Sprite"
-    />
-    <img
-      class="absolute w-auto h-full z-30"
-      :src="`/images/alpaca/eyes/${sprite.eyes || 'default'}.png`"
-      alt="Alpaca Sprite"
-    />
-    <img
-      class="absolute w-auto h-full z-20"
-      :src="`/images/alpaca/hair/${sprite.hair || 'default'}.png`"
-      alt="Alpaca Sprite"
-    />
-    <img
-      class="absolute w-auto h-full"
-      :src="`/images/alpaca/leg/${sprite.leg || 'default'}.png`"
-      alt="Alpaca Sprite"
-    />
-    <img
-      class="absolute w-auto h-full z-10"
-      :src="`/images/alpaca/mouth/${sprite.mouth || 'default'}.png`"
-      alt="Alpaca Sprite"
-    />
-    <img
-      class="absolute w-auto h-full"
-      :src="`/images/alpaca/neck/${sprite.neck || 'default'}.png`"
-      alt="Alpaca Sprite"
-    />
-    <img
-      class="absolute w-auto h-full"
-      :src="`/images/alpaca/nose/${sprite.nose || 'nose'}.png`"
+      :class="{[`z-${part.zIndex}`]: part.zIndex, 'rounded-xl': part.name === 'backgrounds'}"
+      :src="`${IMAGE_PATH}${part.name}/${sprite[part.name] || part.default}.png`"
       alt="Alpaca Sprite"
     />
   </div>
